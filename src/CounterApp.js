a4d2c10bd81556e29e06e3263987ff99f42be69d
German Tellez <gtellez@condorlabs.io>
2021-06-30 09:24:58 -0500

    WIP - Create CounterApp component

---
+++ src/CounterApp.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const CounterApp = ({ value }) => {
+  return (
+    <>
+    <h1> CounterApp </h1>
+    <h2> { value } </h2>
+    </>
+  )
+}
+
